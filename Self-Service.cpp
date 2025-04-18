826a83e65c9d0e8253458197ddc5376b282bb193
Alireza <alirezawaghei80@gmail.com>
2025-04-18 10:15:47 -0700

    added function to Stdent class #phase1

--- Self-Service.cpp
+++ Self-Service.cpp
@@ -3,6 +3,31 @@ using namespace std;
 
 class Student
 {
+private:
+    int user_id;
+    string student_id;
+    string name;
+    string email;
+    float balance;
+    bool is_active;
+
+public:
+    Student();
+    void print();
+    void reserve_meal(Meal);
+    bool cancel_reservation(Rezervation);
+
+    int getUser_id();
+    string getStudent_id();
+    string getName();
+    string getEmail();
+    float getBalance();
+
+    void setUser_id(int);
+    void setStudent_id(string);
+    void setName(string);
+    void setEmail(string);
+    void setBalance(float);
 };
 
 class Rezervation
