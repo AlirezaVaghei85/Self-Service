912c807d4c8067fe7cb504a78fceafd2157e5557
Alireza <alirezawaghei80@gmail.com>
2025-05-08 06:42:17 -0700

    deffined setters and getters of User #phase2

--- Self-Service.cpp
+++ Self-Service.cpp
@@ -20,6 +20,18 @@ private:
     string hashedPassword;
 
 public:
+    virtual void print() const;
+    virtual void getType();
+
+    int getUserID() { return userID; }
+    string getName() { return name; }
+    string getLastName() { return lastName; }
+    string getHashedPassword() { return hashedPassword; }
+
+    void setUserID(int i) { userID = i; }
+    void setName(string n) { name = n; }
+    void setLastName(string n) { lastName = n; }
+    void setHashedPassword(string h) { hashedPassword = h; }
 };
 
 class Admin : public User
