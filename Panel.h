c7b822a049177a49e1ca44a5403270ca23e2fa58
Alireza <alirezawaghei80@gmail.com>
2025-05-18 10:18:11 -0700

    Fixed #phase3

--- Panel.h
+++ Panel.h
@@ -12,7 +12,6 @@ public:
     void showStudentInfo();
     void checkBalance();
     void viewReservations();
-    void addReservation(Reservation);
     void addToShoppingCart();
     void confirmShoppingCart();
     void removeShoppingCartItem();
